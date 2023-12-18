679c066d2bca57ccafa6d5ce68ceb9f054e328cb
guillegiuu <115334202+guillegiuu@users.noreply.github.com>
2023-12-18 22:30:37 +0000

    Mod_2
    Ejercicio 2

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ Empecemos los problemas avanzados probando si un número está dentro de un cier
 
 
 def en_rango(num, inferior, superior):
-    if num >= inferior and num <= superior:
+    if (num >= inferior and num <= superior):
         return True
     else:
         return False
@@ -36,4 +36,26 @@ El operador and realiza una evaluación perezosa (short-circuiting), lo que sign
 
 """
 
-# B
+# B Mismo nombre:
+"""
+Necesitamos escribir un programa que compruebe diferentes nombres y determine si son iguales. Necesitamos aceptar dos cadenas (strings) y compararlas. Aquí están los pasos:
+
+1 - Define la función para que acepte dos cadenas, tu_nombre y mi_nombre.
+
+2 - Comprueba si las dos cadenas son iguales.
+
+3 - Devuelve True si son iguales, en caso contrario devuelve False.
+
+
+"""
+
+
+def mismo_nombre(tu_nombre, mi_nombre):
+    if (tu_nombre == mi_nombre):
+        return True
+    else:
+        return False
+
+
+print(mismo_nombre("guille", "manu"))
+print(mismo_nombre("rober", "rober"))
